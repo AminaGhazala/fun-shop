4e0b0ae954775daac754b689dec5c811be43a9df
Jason Kim <thehesedlife@gmail.com>
2020-07-23 21:17:13 -0700

    updated render method

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -22,27 +22,42 @@ export default class ProductDetails extends React.Component {
   }
 
   render() {
-    const productDetailMenu = (
-      <div className="row justify-content-center">
-        <div className="col text-muted px-4 py-3" style={{ cursor: 'pointer' }} >&lt; Back to catalog</div>
-      </div>
-    );
-
-    const productDetailBody = (
-      <div className="row justify-content-center">
-        <div className="col"></div>
-      </div>
-    );
-
-    const productDetailDesciption = (
-      <div className="row justify-content-center">
-        <div className="col"></div>
-      </div>
-    );
-
     if (this.state.product === null) {
       return null;
     } else {
+      const priceLength = this.state.product.price.toString().length;
+      const price = '$' + this.state.product.price.toString().substr(0, priceLength - 2) + '.' + this.state.product.price.toString().substr(priceLength - 2);
+
+      const productDetailMenu = (
+        <div className="row justify-content-center">
+          <div className="col" style={{ cursor: 'pointer' }} >
+            <p className="text-muted p-4 m-0">&lt; Back to catalog</p>
+          </div>
+        </div>
+      );
+
+      const productDetailBody = (
+        <div className="row justify-content-center">
+          <div className="col-sm-6 col-md-5">
+            <img src={this.state.product.image} className="card-img-top px-4" alt={this.state.product.name}
+              style={{ width: '100%', height: '300px', objectFit: 'contain' }} />
+          </div>
+          <div className="col-sm-6 col-md-7">
+            <h5 className="card-title mb-3"><b>{this.state.product.name}</b></h5>
+            <p className="card-text text-muted mb-3">{price}</p>
+            <p className="card-text">{this.state.product.shortDescription}</p>
+          </div>
+        </div>
+      );
+
+      const productDetailDesciption = (
+        <div className="row justify-content-center">
+          <div className="col">
+            <p className="px-4 py-2">{this.state.product.longDescription}</p>
+          </div>
+        </div>
+      );
+
       return (
         <div className="product-detail-container card m-5" style={{ maxWidth: '97vw' }}>
           {productDetailMenu}
