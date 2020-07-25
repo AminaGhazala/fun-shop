47e2c75b8530dffe315623f8509879904f0ffccd
Jason Kim <thehesedlife@gmail.com>
2020-07-24 21:52:22 -0700

    completed shopping cart updated styles of CartSummaryItem component updated styles of CartSummary component added getTotalPrice method to calculate a total price

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
 export default function CartSummaryItem(props) {
-  const priceLength = this.props.product.price.toString().length;
-  const price = '$' + this.props.product.price.toString().substr(0, priceLength - 2) + '.' + this.props.product.price.toString().substr(priceLength - 2);
+  const priceLength = props.product.price.toString().length;
+  const price = '$' + props.product.price.toString().substr(0, priceLength - 2) + '.' + props.product.price.toString().substr(priceLength - 2);
 
   return (
-    <div className="row justify-content-between align-items-center card">
-      <div className="col-sm-6 col-md-5">
-        <img src={this.props.product.image} className="card-img-top px-4" alt={this.props.product.name}
-          style={{ width: '100%', height: '300px', objectFit: 'contain' }} />
-      </div>
-      <div className="col-sm-6 col-md-7 px-4">
-        <h5 className="card-title mb-3"><b>{this.props.product.name}</b></h5>
-        <p className="card-text text-muted mb-3">{price}</p>
-        <p className="card-text">{this.props.product.shortDescription}</p>
+    <div className="card my-4">
+      <div className="row align-items-center">
+        <div className="col-sm-6 col-md-5">
+          <img src={props.product.image} className="card-img-top p-4" alt={props.product.name}
+            style={{ width: '100%', height: '300px', objectFit: 'contain' }} />
+        </div>
+        <div className="col-sm-6 col-md-7 px-4">
+          <h5 className="card-title mb-3"><b>{props.product.name}</b></h5>
+          <p className="card-text text-muted mb-3">{price}</p>
+          <p className="card-text">{props.product.shortDescription}</p>
+        </div>
       </div>
     </div>
   );
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import CartSummaryItem from './cart-summary-item';
+import CartSummaryItem from './cart-summary-item';
 
 export default class CartSummary extends React.Component {
   constructor(props) {
@@ -11,12 +11,19 @@ export default class CartSummary extends React.Component {
     this.props.selectedView('catalog', {});
   }
 
+  getTotalPrice() {
+    const totalPrice = this.props.cart.reduce((acc, cur) => parseInt(acc) + parseInt(cur.price), 0);
+    if (totalPrice === 0) return null;
+
+    const priceLength = totalPrice.toString().length;
+    return 'Item Total $' + totalPrice.toString().substr(0, priceLength - 2) + '.' + totalPrice.toString().substr(priceLength - 2);
+  }
+
   render() {
-    const tatalAmount = 'Item Total';
     const cartSummaryMenu = (
       <div className="row justify-content-center">
         <div className="col" style={{ cursor: 'pointer' }} onClick={this.handleBack} >
-          <p className="text-muted p-4 m-0">&lt; Back to catalog</p>
+          <p className="text-muted py-4 m-0">&lt; Back to catalog</p>
         </div>
       </div>
     );
@@ -24,25 +31,29 @@ export default class CartSummary extends React.Component {
     const cartSummaryTitle = (
       <div className="row justify-content-center">
         <div className="col">
-          <h1 className="m-0">My Cart</h1>
+          <h1 className="pb-4 m-0">My Cart</h1>
         </div>
       </div>
     );
 
     const cartSummaryItem = (
-      null
+      this.props.cart.length === 0
+        ? <h3 className="pb-4 m-0">Shopping cart is empty.</h3>
+        : this.props.cart.map((item, index) => (
+          <CartSummaryItem product={item} key={index}/>
+        ))
     );
 
     const cartSummaryTotal = (
       <div className="row justify-content-center">
         <div className="col">
-          <h3 className="m-0">{tatalAmount}</h3>
+          <h3 className="m-0">{this.getTotalPrice()}</h3>
         </div>
       </div>
     );
 
     return (
-      <div className="cart-summary-container card m-5" style={{ maxWidth: '97vw' }}>
+      <div className="cart-summary-container m-5 px-4" style={{ maxWidth: '97vw' }}>
         {cartSummaryMenu}
         {cartSummaryTitle}
         {cartSummaryItem}
